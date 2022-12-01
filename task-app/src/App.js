5a40470585a207723ceebc02af10544b815f5f90
vijayalakshmi258 <119560833+vijayalakshmi258@users.noreply.github.com>
2022-12-01 12:16:02 +0530

    Add files via upload

---
+++ task-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TaskContainer from './components/task-container/task-container';
+
+let App = () => (
+  <div className="App">
+    <div className='App-header'>All Tasks List</div>
+    <TaskContainer />
+  </div>
+)
+
+export default App
