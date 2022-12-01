0f9ae6e44cfed8e8ab4845a827d041b8c2f09a7b
vijayalakshmi258 <119560833+vijayalakshmi258@users.noreply.github.com>
2022-12-01 12:24:41 +0530

    Add files via upload

--- task-app/src/components/task/task.js
+++ task-app/src/components/task/task.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import './task.css';
+import moment from 'moment';
 
+export default class Task extends React.Component {
+  state = {
+    id: this.props.id,
+    title: this.props.task.title,
+    dueDate: this.props.task.dueDate,
+    dueTime: this.props.task.dueTime,
+  }
+  UNSAFE_componentWillReceiveProps = (props) => this.setState({
+    id: props.task.id,
+    text: props.task.title,
+    dueDate: props.task.dueDate,
+    dueTime: props.task.dueTime,
+  })
+  render() {
+    return (
+      <div className="task">
+        <div className='title'>{this.state.title}</div>
+        {moment(this.state.dueDate).format('ddd, MMM DD, YYYY. ')}
+        {this.state.dueTime}
+    </div >
+)
+}
+}
