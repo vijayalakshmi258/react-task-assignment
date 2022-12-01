3cdf433e72deaf74cf1805332a506879ed0fafbe
vijayalakshmi258 <119560833+vijayalakshmi258@users.noreply.github.com>
2022-12-01 12:22:11 +0530

    Add files via upload

--- task-app/src/components/task-list/task-list.js
+++ task-app/src/components/task-list/task-list.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import Task from '../task/task';
+import './task-list.css';
 
+export default class TaskList extends React.Component {
+  state = {
+    tasks: this.props.tasks
+  }
+
+  UNSAFE_componentWillReceiveProps = (props) => this.setState({ tasks: props.tasks })
+
+  render() {
+    return (
+   <div>
+    {this.state.tasks.map(task =>
+              <Task
+                key={task.id}
+                id={task.id}
+                task={task} />
+            )}
+   </div>
+)
+}
+}
